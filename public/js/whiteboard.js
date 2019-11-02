0356d7634eee1bb6136ed3890b5eacc90cdf778e
Dan Bruce <dan@ddbruce.com>
2019-11-02 19:54:35 -0400

    work on the frontend

--- public/js/whiteboard.js
+++ public/js/whiteboard.js
@@ -12,7 +12,7 @@ function updateCrew(crew_response) {
         crew.rider2rn = crew.rider2rn == 0 ? rider_radio_nums.pop() : crew.rider2rn;
         document.querySelector('#cc').innerHTML = crew.cc;
         document.querySelector('#ccrn').innerHTML = crew.ccrn;
-        document.querySelector('#driver').innerHTML = crew.driver;
+        // document.querySelector('#driver').innerHTML = crew.driver;
         document.querySelector('#driverrn').innerHTML = crew.driverrn;
         document.querySelector('#rider1').innerHTML = crew.rider1;
         document.querySelector('#rider1rn').innerHTML = crew.rider1rn;
