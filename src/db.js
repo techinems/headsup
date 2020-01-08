b0aaa2cafd12dff297799aeb2dbc6189e7b25b66
Logan Ramos <lramos15@gmail.com>
2020-01-07 20:46:09 -0500

    Fix some code climate issues

--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
-async function execQuery(pool, q, args = null, responseFunc = null, db = 'headsup') {
+async function execQuery(pool, q, args = null, responseFunc = null) {
     let conn;
     try {
         conn = await pool.getConnection();
-        await conn.query(`USE ${db};`);
+        await conn.query(`USE ${process.env.DB_NAME};`);
         const results = await (args != null ? conn.query(q, args) : conn.query(q));
         const response = { success: true };
         if (responseFunc != null) {
