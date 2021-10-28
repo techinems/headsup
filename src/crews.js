b73d5b92cfe83e9bfaffb6a6e0a7de292f0d95a5
tceluzza <tomceluzza@gmail.com>
2021-10-28 15:49:29 -0400

    Dynamically update Duty Sup from crews

--- src/crews.js
+++ src/crews.js
@@ -7,10 +7,12 @@ const CREW_GET = `
         CONCAT(SUBSTRING(d.first_name, 1, 1), '. ', d.last_name) as driver,
         CONCAT(SUBSTRING(r1.first_name, 1, 1), '. ', r1.last_name) as rider1,
         CONCAT(SUBSTRING(r2.first_name, 1, 1), '. ', r2.last_name) as rider2,
+        CONCAT(SUBSTRING(ds.first_name, 1, 1), '. ', ds.last_name) as dutysup,
         c.radionum as ccrn,
         d.radionum as driverrn,
         r1.radionum as rider1rn,
-        r2.radionum as rider2rn
+        r2.radionum as rider2rn,
+        ds.radionum as dutysuprn
     FROM
         (SELECT m.first_name, m.last_name, m.radionum, c.date FROM crews c, members m
             WHERE m.id = c.cc AND c.date = ?) as c,
@@ -20,11 +22,14 @@ const CREW_GET = `
             WHERE m.id = c.attendant AND c.date = ?) as r1,
         (SELECT m.first_name, m.last_name, m.radionum, c.date FROM crews c, members m
             WHERE m.id = c.observer AND c.date = ?) as r2
+        (SELECT m.first_name, m.last_name, m.radionum, c.date FROM crews c, members m
+            WHERE m.id = c.dutysup AND c.date = ?) as ds
     WHERE
         c.date IS NOT NULL AND
         d.date IS NOT NULL AND
         r1.date IS NOT NULL AND
-        r2.date IS NOT NULL;
+        r2.date IS NOT NULL AND
+        ds.date IS NOT NULL;
 `;
 
 function buildDateArray() {
